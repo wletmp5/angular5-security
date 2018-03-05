bca9cf52923d0ea4823d6c469c1aee2b447af40b
glenndennismorales <wletmp5@yahoo.com>
2018-03-04 23:26:29 -0500

    completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
+import {BrowserModule} from '@angular/platform-browser';
 import {NgModule, Provider} from '@angular/core';
 import {HttpClientModule, HttpClientXsrfModule} from '@angular/common/http';
 
-import { AppComponent } from './app.component';
-import { LessonsComponent } from './lessons/lessons.component';
-import { LoginComponent } from './login/login.component';
-import { SignupComponent } from './signup/signup.component';
-import {routesConfig} from "./routes.config";
-import {LessonsService} from "./services/lessons.service";
-import {ReactiveFormsModule} from "@angular/forms";
+import {AppComponent} from './app.component';
+import {LessonsComponent} from './lessons/lessons.component';
+import {LoginComponent} from './login/login.component';
+import {SignupComponent} from './signup/signup.component';
+import {routesConfig} from './routes.config';
+import {LessonsService} from './services/lessons.service';
+import {ReactiveFormsModule} from '@angular/forms';
 
-import {AuthService} from "./services/auth.service";
-import { AdminComponent } from './admin/admin.component';
-import { RouterModule} from "@angular/router";
+import {AuthService} from './services/auth.service';
+import {AdminComponent} from './admin/admin.component';
+import {Router, RouterModule} from '@angular/router';
 
 
 import 'rxjs/add/operator/switchMap';
@@ -23,8 +23,8 @@ import 'rxjs/add/operator/filter';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/first';
 import 'rxjs/add/observable/of';
-
-
+import {RbacAllowDirective} from './common/rbac-allow.directive';
+import {AuthorizationGuard} from './services/authorization.guard';
 
 
 @NgModule({
@@ -33,24 +33,37 @@ import 'rxjs/add/observable/of';
     LessonsComponent,
     LoginComponent,
     SignupComponent,
-    AdminComponent
+    AdminComponent,
+    RbacAllowDirective
   ],
   imports: [
     BrowserModule,
-      HttpClientModule,
-      HttpClientXsrfModule.withOptions({
-         cookieName: 'XSRF-TOKEN',
-         headerName:  'x-xsrf-token'
-      }),
-      RouterModule.forRoot(routesConfig),
-      ReactiveFormsModule
+    HttpClientModule,
+    HttpClientXsrfModule.withOptions({
+      cookieName: 'XSRF-TOKEN',
+      headerName: 'x-xsrf-token'
+    }),
+    RouterModule.forRoot(routesConfig),
+    ReactiveFormsModule
   ],
   providers: [
-      LessonsService,
-      AuthService
+    LessonsService,
+    AuthService,
+    {
+      provide: 'adminsOnlyGuard',
+      useFactory: (authService: AuthService,
+                   router: Router) =>
+        new AuthorizationGuard(['ADMIN'], authService, router),
+      deps: [
+        AuthService,
+        Router
+      ]
+
+    }
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent];
 })
+
 export class AppModule {
 
 }
